b69990515af4d6db5ba5808794990f8e9102fae3
Juraj Kirchheim <back2dos@googlemail.com>
2014-08-27 11:06:05 +0200

    Don't mix tabs and spaces.

--- src/tink/geom2/Point.hx
+++ src/tink/geom2/Point.hx
@@ -4,22 +4,22 @@ using tink.CoreApi;
 
 abstract Point(Pair<Float, Float>) {
   public var x(get, never):Float;
-	public var y(get, never):Float;
-	
-	inline function get_x() return this.a;
-	inline function get_y() return this.b;
-	
-	public inline function new(x, y) this = new Pair(x, y);
-	
-	@:to public inline function toString() 
-		return '($x, $y)';		
-	
-	@:op(a + b) static public inline function add(a:Point, b:Point)
-		return new Point(a.x + b.x, a.y + b.y);
+  public var y(get, never):Float;
+  
+  inline function get_x() return this.a;
+  inline function get_y() return this.b;
+  
+  public inline function new(x, y) this = new Pair(x, y);
+  
+  @:to public inline function toString() 
+    return '($x, $y)';    
+  
+  @:op(a + b) static public inline function add(a:Point, b:Point)
+    return new Point(a.x + b.x, a.y + b.y);
 
-	@:op(a - b) static public inline function subtract(a:Point, b:Point)
-		return new Point(a.x - b.x, a.y - b.y);
+  @:op(a - b) static public inline function subtract(a:Point, b:Point)
+    return new Point(a.x - b.x, a.y - b.y);
 
-	@:op(-p) static public inline function invert(p:Point)
-		return new Point(-p.x, -p.y);
+  @:op(-p) static public inline function invert(p:Point)
+    return new Point(-p.x, -p.y);
 }
