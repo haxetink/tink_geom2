21a2ca69657bfadaede3ddfadb174afda4876198
Juraj Kirchheim <back2dos@googlemail.com>
2016-03-28 18:12:08 +0200

    Fix indenting.

--- src/tink/geom2/Point.hx
+++ src/tink/geom2/Point.hx
@@ -17,14 +17,14 @@ abstract Point(Pair<Float, Float>) from Pair<Float, Float> to Pair<Float, Float>
   public inline function new(x, y) this = new Pair(x, y);
   
   public inline function normalize(l:Float = 1):Point 
-    return scale(this, l / length);	  
+    return scale(this, l / length);    
   
   public inline function dot(that:Point):Float
     return x * that.x + y * that.y; 
 
   public inline function isLeftOf(from:Point, to:Point)
     return (to.x - from.x) * (y - from.y) > (to.y - from.y) * (x - from.x);  
-		
+    
   @:to public inline function toString() 
     return '($x, $y)';    
   
@@ -41,5 +41,5 @@ abstract Point(Pair<Float, Float>) from Pair<Float, Float> to Pair<Float, Float>
     return new Point(p.x * f, p.y * f);
   
   @:from static inline function ofObj(obj: { var x(default, null):Float; var y(default, null):Float; }):Point
-	return new Point(obj.x, obj.y);
+    return new Point(obj.x, obj.y);
 }
