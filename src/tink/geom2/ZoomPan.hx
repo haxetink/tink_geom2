f0c1cb8f00ca7f46592f6077e8374f672134d4cc
Juraj Kirchheim <back2dos@googlemail.com>
2020-05-14 11:31:55 +0200

    Add various ops to ZoomPan.

--- src/tink/geom2/ZoomPan.hx
+++ src/tink/geom2/ZoomPan.hx
@@ -19,7 +19,25 @@ abstract ZoomPan(ZoomPanData) {
   public inline function new(scale, tx, ty)
     this = new ZoomPanData(scale, tx, ty);
 
-  @:op(m * n) static public function concat(m:ZoomPan, n:ZoomPan)
+  @:commutative @:op(m * p) static public function transformPoint(t:ZoomPan, p:Point)
+    return new Point(t.scale * p.x + t.tx, t.scale * p.y + t.ty);
+
+  @:op(m * f) static function scale(m:ZoomPan, f:Float) //TODO: all these ops are easy enough to inline instead of requiring a generic concat
+    return m * (f:ZoomPan);
+
+  @:op(f * m) static function rscale(f:Float, m:ZoomPan)
+    return (f:ZoomPan) * m;
+
+  @:op(m + p) static function move(m:ZoomPan, p:Point)
+    return m * (p:ZoomPan);
+
+  @:op(p + m) static function rmove(p:Point, m:ZoomPan)
+    return (p:ZoomPan) * m;
+
+  @:op(m - p) static inline function moveback(m:ZoomPan, p:Point)
+    return move(m, -p);
+
+  @:op(m * n) static function concat(m:ZoomPan, n:ZoomPan)
     return new ZoomPan(
       n.scale * m.scale,
       n.scale * m.tx + n.tx,
@@ -29,16 +47,13 @@ abstract ZoomPan(ZoomPanData) {
   @:to public function toString()
     return 'scale(${this.scale}) translate(${this.tx}px, ${this.ty}px)';
 
-  @:commutative @:op(m * p) static public function transformPoint(t:ZoomPan, p:Point)
-    return new Point(t.scale * p.x + t.tx, t.scale * p.y + t.ty);
-
   @:to public inline function toMatrix():Matrix
     return new Matrix(this.scale, 0, 0, this.scale, this.tx, this.ty);
 
   @:from static public inline function zoom(f:Float)
     return new ZoomPan(f, 0, 0);
 
-  static public inline function pan(p:Point)
+  @:from static public inline function pan(p:Point)
     return new ZoomPan(1, p.x, p.y);
 
 }
