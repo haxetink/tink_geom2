1f2b7749ed5f1d04e212971274598341f391a085
Juraj Kirchheim <back2dos@googlemail.com>
2020-05-14 09:49:58 +0200

    Bump travix. Setup tink_unittest. Update vscode setup.

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -4,8 +4,15 @@ import tink.geom2.Matrix;
 import tink.geom2.Point;
 import tink.geom2.Rect;
 
+import tink.unit.TestBatch;
+import tink.testrunner.*;
+
 class RunTests {
   static function main() {
-    travix.Logger.exit(0);
+    Runner.run(
+      TestBatch.make(
+
+      )
+    ).handle(Runner.exit);
   }
 }
